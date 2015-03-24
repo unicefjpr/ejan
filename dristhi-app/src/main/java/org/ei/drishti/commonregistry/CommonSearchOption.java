f7b422445140704615d33e775ed74cce56604930
raihan-mpower <raihan@mpower-social.com>
2015-03-24 15:34:55 +0600

    RA|changes added to add a common search option

--- dristhi-app/src/main/java/org/ei/drishti/commonregistry/CommonSearchOption.java
+++ dristhi-app/src/main/java/org/ei/drishti/commonregistry/CommonSearchOption.java
@@ -6,6 +6,9 @@ import org.ei.drishti.R;
 import org.ei.drishti.view.contract.SmartRegisterClient;
 import org.ei.drishti.view.dialog.FilterOption;
 
+import java.util.Iterator;
+import java.util.Map;
+
 public class CommonSearchOption implements FilterOption {
     private final String criteria;
 
@@ -15,11 +18,43 @@ public class CommonSearchOption implements FilterOption {
 
     @Override
     public String name() {
-        return Context.getInstance().getStringResource(R.string.str_ec_search_hint);
+        return Context.getInstance().getStringResource(R.string.common_register_search_hint);
     }
 
     @Override
     public boolean filter(SmartRegisterClient client) {
-        return StringUtils.isBlank(criteria) || client.satisfiesFilter(criteria);
+        CommonPersonObjectClient cpoc = (CommonPersonObjectClient) client;
+        boolean isthere = false;
+        if (cpoc.getCaseId().contains(criteria)) {
+            isthere = true;
+        } else {
+            for (int i = 0; i < cpoc.getColumnmaps().size(); i++) {
+                Iterator it = cpoc.getColumnmaps().entrySet().iterator();
+                while (it.hasNext()) {
+                    Map.Entry pair = (Map.Entry) it.next();
+                    if (criteria.contains("" + pair.getValue())) {
+                        isthere = true;
+                        break;
+                    }
+                    it.remove(); // avoids a ConcurrentModificationException
+                }
+
+            }
+            if (!isthere) {
+                for (int i = 0; i < cpoc.getDetails().size(); i++) {
+                    Iterator it = cpoc.getDetails().entrySet().iterator();
+                    while (it.hasNext()) {
+                        Map.Entry pair = (Map.Entry) it.next();
+                        if (criteria.contains("" + pair.getValue())) {
+                            isthere = true;
+                            break;
+                        }
+                        it.remove(); // avoids a ConcurrentModificationException
+                    }
+                }
+            }
+
+        }
+        return isthere;
     }
 }
